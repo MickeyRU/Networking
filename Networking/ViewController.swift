13aeb34159f72ffb627fcf5c35ab0798cabc99a3
Pavel Afanasiev <afanasyevpv9@gmail.com>
2022-09-08 13:52:53 +0300

    Загрузка изображения из сети

--- Networking/ViewController.swift
+++ Networking/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var buttonForLoadImage: UIButton!
+    @IBOutlet weak var imageForLoad: UIImageView!
+    @IBOutlet weak var labelForUserAction: UILabel!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        activityIndicator.isHidden = true
+        activityIndicator.hidesWhenStopped = true
+        
     }
-
-
+    
+    @IBAction func buttonPressed() {
+        labelForUserAction.isHidden = true
+        buttonForLoadImage.isHidden = true
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        
+        guard let url = URL(string: "https://i.artfile.ru/1920x1200_1072880_[www.ArtFile.ru].jpg") else { return }
+         
+        let session = URLSession.shared
+        
+        session.dataTask(with: url) { (data, responce, error) in
+            if let data = data, let image = UIImage(data: data) {
+                DispatchQueue.main.async {
+                    self.activityIndicator.stopAnimating()
+                    self.imageForLoad.image = image
+                }
+            }
+        }.resume()
+    }
 }
 

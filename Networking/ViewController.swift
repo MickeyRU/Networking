5187931b8789c97b63dc5f48d670a163502040bf
Pavel Afanasiev <afanasyevpv9@gmail.com>
2022-09-09 10:29:46 +0300

    post request

--- Networking/ViewController.swift
+++ Networking/ViewController.swift
@@ -15,16 +15,15 @@ class ViewController: UIViewController {
     
     @IBAction func getButtonPushed() {
         
-        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts/1") else { return }
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
         
         let session = URLSession.shared
         session.dataTask(with: url) { (data, response, error) in
             if let data = data, let response = response {
-                print(response)
                 print(data)
-                
+                print(response)
                 do {
-                    // Сюриализация данных полученных от сервера в json формат для последующего размещения
+                    // Сериализация данных полученных от сервера в json формат для последующего размещения
                     let json = try JSONSerialization.jsonObject(with: data, options: [] )
                     print(json)
                     
@@ -36,7 +35,37 @@ class ViewController: UIViewController {
     }
     
     
-    @IBAction func poshButtonPushed() {
+    @IBAction func postButtonPushed() {
+        
+        // Адресс с которым взаимодействуем
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+
+        // Данные которые мы отправляем на сервер
+        let userData = ["Course": "Networking", "Student": "Pavel Afanasyev"]
+        
+        // Запрос и его настройка
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: userData, options: []) else { return }
+        request.httpBody = httpBody
+        request.addValue("application/json ", forHTTPHeaderField: "Content-Type")
+        
+        let session = URLSession.shared
+        session.dataTask(with: request) { (data, response, error) in
+            
+            guard let data = data, let response = response else { return }
+            
+            print(response)
+
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                
+                print(json)
+                
+            } catch let error as NSError {
+                print(error.localizedDescription)
+            }
+        }.resume()
     }
-    
 }
